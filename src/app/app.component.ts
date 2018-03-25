acef6305947af9d1ecc3ffd91454cd007ad1196e
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-25 22:18:46 +0530

    create custom template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, AfterContentInit, ComponentRef } from '@angular/core';
-import { DynamicComponent } from './dynamic/dynamic.component';
+import { Component, AfterContentInit, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,30 +6,9 @@ import { DynamicComponent } from './dynamic/dynamic.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterContentInit {
-  component: ComponentRef<DynamicComponent>;
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-
-  constructor(
-    private resolver: ComponentFactoryResolver
-  ) {}
-
+  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
+  @ViewChild('tmp') tmp: TemplateRef<any>;
   ngAfterContentInit() {
-    const dynamicComponent = this.resolver.resolveComponentFactory(DynamicComponent);
-    this.entry.createComponent(dynamicComponent); // default order -1
-    this.component = this.entry.createComponent(dynamicComponent, 0);
-    this.component.instance.title = 'Updated ' + this.component.instance.title;
-    this.component.instance.submitted.subscribe(this.loginUser);
-  }
-
-  destroyComponent() {
-    this.component.destroy();
-  }
-
-  moveComponent() {
-    this.entry.move(this.component.hostView, 1);
-  }
-
-  loginUser(user) {
-    console.log('Login', user);
+    this.entry.createEmbeddedView(this.tmp);
   }
 }
