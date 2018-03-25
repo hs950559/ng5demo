44a29c1e7d15ef8e42fc21a24f2f268d9ad634db
Hemant Kumar Singh <hemantkumarsingh@Hemants-MacBook-Air.local>
2018-03-25 22:38:38 +0530

    pass data to custom template ( better way )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  ctx = {
+    $implicit: 'Varun Gupta',
+    location: 'India, Hyderabad'
+  };
 }
